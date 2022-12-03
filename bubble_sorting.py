1718908437c596aed4a6413a63be1f46d8e0dbdf
Oleg <89175955650@mail.ru>
2022-12-03 14:56:25 +0300

    Finish commit for bubble_sorting.py.

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -1,24 +1,24 @@
-import random
 import datetime
 from random import shuffle
 
-list_items = [item for item in range(5)]
+list_items = [item for item in range(1000)]
 shuffle(list_items)
 print(f"The random_start_list: \n{list_items}.")
 
-new_sorting_list = []
 start_time = datetime.datetime.now()
-while list_items != []:
+
+last_index = len(list_items) - 1
+
+while last_index >= 0:
     first_index = 0
-    second_index = first_index + 1
-    while first_index <= (len(list_items) - 2):
-        if list_items[first_index] < list_items[second_index]:
-            first_index += 1
-            continue
-        else:
+    while first_index <= last_index - 1:
+        second_index = first_index + 1
+        if list_items[first_index] > list_items[second_index]:
             list_items[first_index], list_items[second_index] = list_items[second_index], list_items[first_index]
         first_index += 1
-    new_sorting_list.append(list_items[-1])
-    list_items = list_items[:-1]
-# new_sorting_list = new_sorting_list[::-1]
-print(f"New sorting list: \n{new_sorting_list}")
+    last_index -= 1
+
+total_time = datetime.datetime.now() - start_time
+
+print(f"New sorting list: \n{list_items}")
+print(f"Total executing time: {total_time}.")
